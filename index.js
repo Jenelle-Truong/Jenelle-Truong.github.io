263cd845cf3331d9c3154f11b45f9f43a5518797
Jenelle Truong <jftruong@ucsd.edu>
2022-08-14 23:38:48 -0700

    typing out name works; still working on typing out footer title

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-const textNameSpan = document.querySelector(".type-name")
 const cursorSpan = document.querySelector(".cursor");
+const textNameSpan = document.querySelector(".type-name");
+const textFooterSpan = document.querySelector(".type-footer")
 
-const typingDelay = 200;
-const erasingDelay = 100;
-const cycleDelay = 2000;
+const typingDelay = 250;
+const erasingDelay = 250;
+const cycleDelay = 3000;
 let charIndex = 0;
 
-function type(textToType) {
+function type(textToType, typeText) {
     if (charIndex < textToType.length) {
         if (!cursorSpan.classList.contains("typing")) {
             cursorSpan.classList.add("typing");
         }
-        textNameSpan.textContent += textToType.charAt(charIndex);
+        if (typeText === "name") {
+            textNameSpan.textContent += textToType.charAt(charIndex);
+        } else if (typeText === "type-footer") {
+            textFooterSpan += textToType.charAt(charIndex);
+        }
         charIndex += 1;
-        setTimeout(type, typingDelay, textToType);
+        setTimeout(type, typingDelay, textToType, typeText);
     } else {
         cursorSpan.classList.remove("typing");
-        setTimeout(erase, cycleDelay, textToType);
+        setTimeout(erase, cycleDelay, textToType, typeText);
     }
 }
 
-function erase(textToType) {
+function erase(textToType, typeText) {
     if (charIndex > 0) {
         if (!cursorSpan.classList.contains("typing")) {
             cursorSpan.classList.add("typing");
         }
-        textNameSpan.textContent = textToType.substring(0, charIndex - 1);
+        if (typeText === "name") {
+            textNameSpan.textContent = textToType.substring(0, charIndex - 1);
+        } else if (typeText === " type-footer") {
+            textFooterSpan.textContent = textToType.substring(0, charIndex - 1);
+        }
         charIndex -= 1;
-        setTimeout(erase, erasingDelay, textToType);
+        setTimeout(erase, erasingDelay, textToType, typeText);
     } else {
         cursorSpan.classList.remove("typing");
-        setTimeout(type, typingDelay + 1100, textToType);
+        setTimeout(type, cycleDelay, textToType, typeText);
     }
 }
 
-function typeIntro() {
-    
-}
+/*document.addEventListener("DOMContentLoaded", function() {
+    type("jenelle truong", "name");
+})*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    type("hello!", "type-footer");
+})
