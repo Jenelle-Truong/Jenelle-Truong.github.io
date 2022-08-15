1bb2d93e32118a71ee264a02190e762135de7262
Jenelle Truong <jftruong@ucsd.edu>
2022-08-15 00:46:13 -0700

    javascript typing effect works

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
 const cursorSpan = document.querySelector(".cursor");
 const textNameSpan = document.querySelector(".type-name");
-const textFooterSpan = document.querySelector(".type-footer")
+const textFooterSpan = document.querySelector(".type-footer");
 
 const typingDelay = 250;
 const erasingDelay = 250;
 const cycleDelay = 3000;
-let charIndex = 0;
 
-function type(textToType, typeText) {
-    if (charIndex < textToType.length) {
-        if (!cursorSpan.classList.contains("typing")) {
-            cursorSpan.classList.add("typing");
+function type(textToType, typeOfSpan) {
+    let charIndex = 0;
+    return new Promise(resolve => {
+        const typeChar = () => {
+            if (charIndex < textToType.length) {
+                if (!cursorSpan.classList.contains("typing")) {
+                    cursorSpan.classList.add("typing");
+                }
+
+                if (typeOfSpan === "name") {
+                    textNameSpan.textContent += textToType.charAt(charIndex);
+                } else if (typeOfSpan === "footer") {
+                    textFooterSpan.textContent += textToType.charAt(charIndex);
+                }
+
+                charIndex += 1;
+                setTimeout(typeChar, typingDelay, textToType, typeOfSpan);
+            } else {
+                cursorSpan.classList.remove("typing");
+                resolve();
+            }                
         }
-        if (typeText === "name") {
-            textNameSpan.textContent += textToType.charAt(charIndex);
-        } else if (typeText === "type-footer") {
-            textFooterSpan += textToType.charAt(charIndex);
-        }
-        charIndex += 1;
-        setTimeout(type, typingDelay, textToType, typeText);
-    } else {
-        cursorSpan.classList.remove("typing");
-        setTimeout(erase, cycleDelay, textToType, typeText);
-    }
+        typeChar();
+    })
 }
 
-function erase(textToType, typeText) {
-    if (charIndex > 0) {
-        if (!cursorSpan.classList.contains("typing")) {
-            cursorSpan.classList.add("typing");
-        }
-        if (typeText === "name") {
-            textNameSpan.textContent = textToType.substring(0, charIndex - 1);
-        } else if (typeText === " type-footer") {
-            textFooterSpan.textContent = textToType.substring(0, charIndex - 1);
-        }
-        charIndex -= 1;
-        setTimeout(erase, erasingDelay, textToType, typeText);
-    } else {
-        cursorSpan.classList.remove("typing");
-        setTimeout(type, cycleDelay, textToType, typeText);
-    }
+async function typeElements() {
+    await type("jenelle truong", "name");
+    await type("let's stay connected", "footer");
 }
 
-/*document.addEventListener("DOMContentLoaded", function() {
-    type("jenelle truong", "name");
-})*/
-
-document.addEventListener("DOMContentLoaded", function() {
-    type("hello!", "type-footer");
-})
+typeElements();
